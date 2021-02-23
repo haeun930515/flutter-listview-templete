86c35f430dc9fa78470136f38d602ba52c6bd466
HAEUN LEE <tnt994@estorm.co.kr>
2021-02-23 17:52:17 +0900

    Adding comments for some components

--- lib/Home/homepage.dart
+++ lib/Home/homepage.dart
@@ -7,6 +7,7 @@ class Homepage extends StatelessWidget {
   Widget build(BuildContext context) {
     return new Scaffold(
       body: new Column(
+        //앱 화면 구성으로, Appbar 아래 body가 나올 수 있도록 구성
         children: <Widget>[new Appbar("app bar"), new Listbody()],
       ),
     );
@@ -19,9 +20,11 @@ class Appbar extends StatelessWidget {
   Appbar(this.title);
   @override
   Widget build(BuildContext context) {
+    //Status bar의 height를 계산
     final double statusbar = MediaQuery.of(context).padding.top;
 
     return new Container(
+        // 윗부분 status bar의 높이만큼 아래로 Appbar를 이동
         padding: new EdgeInsets.only(top: statusbar),
         height: statusbar + 50,
         color: Colors.grey,
--- lib/List/Listcard.dart
+++ lib/List/Listcard.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:listviewtemplete/List/List.dart';
 
 class Listcard extends StatelessWidget {
+  //
   final Item item;
 
   Listcard(this.item);
@@ -9,11 +10,14 @@ class Listcard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return new Container(
+      // List item의 box의 CSS 변경 가능
       margin: const EdgeInsets.only(top: 16, bottom: 16, left: 24, right: 24),
       decoration: new BoxDecoration(
           color: Colors.black12,
           shape: BoxShape.rectangle,
           borderRadius: new BorderRadius.circular(8)),
+
+      // List item의 내부 widget
       child: new Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: <Widget>[
